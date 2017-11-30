f8e38f13646df5027cfded4f8201d90de5fe8cf4
mistertandon <enggparveshtandon@gmail.com>
2017-11-30 21:00:34 +0530

    Developed basic Spring application Spring19AutowiringByName to demonstrate how we can use Autowiring byName concept.

---
+++ Spring18AutowiringByName/src/com/mistertandon/model/PanCard.java
@@ -0,0 +1,26 @@
+package com.mistertandon.model;
+
+public class PanCard {
+	
+	private String panHolderName;
+	
+	private String panNumber;
+
+	public String getPanHolderName() {
+		return panHolderName;
+	}
+
+	public void setPanHolderName(String panHolderName) {
+		this.panHolderName = panHolderName;
+	}
+
+	public String getPanNumber() {
+		return panNumber;
+	}
+
+	public void setPanNumber(String panNumber) {
+		this.panNumber = panNumber;
+	}
+	
+	
+}
