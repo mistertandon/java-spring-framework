2a72d5da892f14dbf349e091c8661ac6d8dcdd7c
mistertandon <enggparveshtandon@gmail.com>
2017-12-06 13:22:06 +0530

    Developed basic spring application Spring41JdbcDAOUsingXML to demonstrate how we can extend jdbcDaoSupport class in DAOClassImplementation, and configure jdbc property using XML configuration.

---
+++ Spring41JdbcDAOUsingXML/src/com/mistertandon/dao/impl/EmployeeDAOImpl.java
@@ -0,0 +1,123 @@
+package com.mistertandon.dao.impl;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.List;
+
+import javax.sql.DataSource;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.support.JdbcDaoSupport;
+
+import com.mistertandon.dao.EmployeeDAO;
+import com.mistertandon.model.Employee;
+
+public class EmployeeDAOImpl extends JdbcDaoSupport implements EmployeeDAO {
+
+	/**
+	 * Reference code: #createEmployeeEDAO
+	 */
+	@Override
+	public void createEmployeeEDAO(Employee employee) {
+
+		int addedEmpId = 0;
+		String sql = null;
+
+		sql = "INSERT INTO `employee` (`name`, `email`, `gender`, `salary`) VALUES (?,?,?,?)";
+		addedEmpId = getJdbcTemplate().update(sql,
+				new Object[] { employee.getName(), employee.getEmail(), employee.getGender(), employee.getSalary() });
+
+		if (addedEmpId > 0) {
+			System.out.println("Employee information has been save in database.");
+		} else {
+			System.out.println("Something went wrong please try after some time.");
+		}
+	}
+
+	/*
+	 * Reference code: #createEmployeeEDAO
+	 * 
+	 * Following function definition can also be used to create new Employee record
+	 * in `employee` table.
+	 * 
+	 * @Override public void createEmployeeEDAO(Employee employee) {
+	 * 
+	 * Connection connectionObj = null; PreparedStatement psObj = null; String sql =
+	 * null; int prepareStatementResponse = 0;
+	 * 
+	 * try { connectionObj = dataSourceObj.getConnection();
+	 * 
+	 * sql =
+	 * "INSERT INTO `employee` (`name`, `email`, `gender`, `salary`) VALUES (?,?,?,?)"
+	 * ;
+	 * 
+	 * psObj = connectionObj.prepareStatement(sql); psObj.setString(1,
+	 * employee.getName()); psObj.setString(2, employee.getEmail());
+	 * psObj.setString(3, employee.getGender()); psObj.setDouble(4,
+	 * employee.getSalary());
+	 * 
+	 * prepareStatementResponse = psObj.executeUpdate();
+	 * 
+	 * if (prepareStatementResponse > 0) {
+	 * 
+	 * System.out.println("Employee information has been save in database."); } else
+	 * { System.out.println("Something went wrong please try after some time."); }
+	 * 
+	 * } catch (Exception e) {
+	 * 
+	 * e.printStackTrace(); } finally {
+	 * 
+	 * if (connectionObj != null) { try { connectionObj.close(); } catch
+	 * (SQLException e) {
+	 * 
+	 * e.printStackTrace(); } } } }
+	 */
+
+	@Override
+	public Employee getEmployeeByIdEDAO(int id) {
+
+		String sql = "SELECT `id`, `name`, `email`, `gender`, `salary` FROM `employee` WHERE id=?";
+		Employee employeeObj = getJdbcTemplate().queryForObject(sql, new EmployeeRowMapper(), id);
+
+		return employeeObj;
+	}
+
+	@Override
+	public void deleteEmployeeByIdEDAO(int id) {
+
+		String sql = null;
+		int returnedId = 0;
+
+		sql = "DELETE FROM `employee` WHERE `employee`.`id` = ?";
+		returnedId = getJdbcTemplate().update(sql, id);
+
+		if (returnedId > 0) {
+			System.out.println("Employee information has been deleted.");
+		}
+	}
+
+	@Override
+	public void updateEmployeeEmailByIdEDAO(String emailId, int employeeId) {
+
+		String sql = null;
+		int returnedId = 0;
+
+		sql = "UPDATE `employee` SET `email` = ? WHERE `employee`.`id` = ?";
+		returnedId = getJdbcTemplate().update(sql, emailId, employeeId);
+
+		if (returnedId > 0) {
+			System.out.println("Email id has been updated.");
+		}
+	}
+
+	@Override
+	public List<Employee> getAllEmployeesDetailsEDAO() {
+
+		String sql = "SELECT `id`, `name`, `email`, `gender`, `salary` FROM `employee` WHERE 1";
+
+		return getJdbcTemplate().query(sql, new EmployeeRowMapper());
+
+	}
+
+}
